447d54e2720171f216322aa08b630780503cd48c
hensolo20 <henockyohak@gmail.com>
2020-07-14 21:15:44 -0700

    express,express-handlebars and body-parser

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-const cars = require("./data.js");
-//get all list of car
-let listCars = cars.getAll();
-//web server on port 3000 and respond the request
+'use strict'
 
-const http = require("http");
-http.createServer((req,res) => {
-  const path = req.url.toLowerCase();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('This is my Home page \n' + 'Number of Cars:'+ listCars.length);
-      break;
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page \n' + 'this is all about my page');
-      break;
 
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Page Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+// send static file as response
+app.get('/', (req, res) => {
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/home.html'); 
+ });
  
+ // send plain text response
+ app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('About page');
+ });
+ // define 404 handler
+ app.use( (req,res) => {
+  res.type('text/plain'); 
+  res.status(404);
+  res.send('404 - Not found');
+ });
+
+ app.listen(app.get('port'), () => {
+  console.log('Express started'); 
+ });
+
+ app.get('/foo', (req,res,next) => {
+  if(Math.random() < 0.5) return next();
+  res.send('sometimes this');
+});
+
+app.get('/foo', (req,res) => {
+  res.send('and sometimes that');
+}); 
+
+
+app.get('/user(user)?', (req,res) => {
+  res.render('user');
+});
+
+app.get('/user/:user', (req, res) => {
+  var info = users.find((user) => {
+    return user.name = req.params.name;
+  });
+  if (!info) return next(); // will eventually fall through to 404
+    res.send(info);
+});
+
+module.exports = function(app) {
+  app.get('/', (req,res) => {
+    app.render('home');
+  });
+
+  // other routes
+};
+
+
